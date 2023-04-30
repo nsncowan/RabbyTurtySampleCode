98ac38e87e1cc193604dfc8289c0f02909478ccb
Noah Cowan <nsncowan@gmail.com>
2023-04-30 09:14:45 -0700

    save

---
+++ RabbyTurty/Button.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+  // this is the door or whatever you want the button to affect.
+  public GameObject objectToSwitch; 
+  
+  // self-explanatory, but this is the class that actually renders the sprite in the environment. This will be re-assigned to the "pressed button" sprite when the player interacts with the button.
+  private SpriteRenderer spriteRenderer; 
+  public Sprite downButton; 
+  private bool hasSwitched; 
+
+  void Start() 
+  {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+  }
+
+  void Update()
+  {
+    
+  }
+
+// in this instance, "other" is the player or whatever we want to assign as interacting with the button.
+  private void OnTriggerEnter2D(Collider2D other) 
+  {
+    if (other.tag == "Player" && !hasSwitched)
+    {
+      objectToSwitch.SetActive(false);
+      spriteRenderer.sprite = downButton;
+      hasSwitched = true;
+    }
+  }
+}
---
+++ RabbyTurty/Player.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed;
+  public Rigidbody2D rigidBody;
+  public float jumpForce;
+  private bool isGrounded;
+  public Transform groundCheckPoint;
+  public LayerMask whatIsGround;
+  private bool canDoubleJump;
+
+  private Animator anim;
+  private SpriteRenderer theSR;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    anim = GetComponent<Animator>();
+    theSR = GetComponent<SpriteRenderer>();
+  }
+
+  void Update()
+  {
+    rigidBody.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), rigidBody.velocity.y);
+    isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
+    if(isGrounded)
+    {
+      canDoubleJump = true;
+    }
+    if(Input.GetButtonDown("Jump"))
+    {
+      if(isGrounded)
+      {
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+      }
+      else
+      {
+        if(canDoubleJump)
+        {
+          rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+          canDoubleJump = false;
+        }
+      }
+    }
+
+// ========================switches direction player is facing based on movement=============================================
+    if(rigidBody.velocity.x < 0)
+    {
+      theSR.flipX = true;
+    }
+    else if(rigidBody.velocity.x > 0)
+    {
+      theSR.flipX = false;
+    }
+
+// ================enables running animation when moving horizontally=====================================================
+    anim.SetFloat("moveSpeed", Mathf.Abs(rigidBody.velocity.x));
+    anim.SetBool("isGrounded", isGrounded);
+  }
+
+// =============tethers player position to platform once you land on platform========================================================
+  private void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Platform")
+    {
+      transform.parent = other.transform;
+    }
+  }
+// ============removes connection to platform once you jump off=========================================================
+  private void OnCollisionExit2D(Collision2D other)
+  {
+    if (other.gameObject.tag == "Platform")
+    {
+      transform.parent = null;
+    }
+  }
+}
